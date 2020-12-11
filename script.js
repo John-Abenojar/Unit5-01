33ad6b90690379cc21365873ec295c863f40ce6b
JohnAbenojar <john.kirby.abenojar@ocsbstudent.ca>
2020-12-11 21:39:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var lol=prompt("Enter your shape")
+if(lol==="trapezoid"){
+    var tr1=prompt("Enter the top base.")
+    var tr2=prompt("Enter the bottom base.")
+    var tr3=prompt("Enter the height.")
+    confirm((tr1+tr2)*(tr3)/2)
+}
